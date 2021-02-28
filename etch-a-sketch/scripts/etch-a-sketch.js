692322683d7b5ff39e054e8977d6edf847c980b9
Michael Braley <michael.parker.braley@gmail.com>
2021-02-28 15:25:26 -0600

    Add document node/elements
    Added variables for the grid container div and a button which allows the user to set the grid size. Made several inline style changes to the h1 element.

--- etch-a-sketch/scripts/etch-a-sketch.js
+++ etch-a-sketch/scripts/etch-a-sketch.js
@@ -1,5 +1,14 @@
-function drawDiv(userInput) {
-    for (i = 0; i < userInput; i++){
-        document.createElement(tagName: x+1);
-    }
+const gridContainer = document.querySelector("#grid");
+const gridSizeSet = document.querySelector("#set_grid");
+
+gridSizeSet.addEventListener("click", setGridSize);
+
+function setGridSize() {
+  let newSize = prompt("Set the board size");
+  if (newSize < 1 || newSize > 64) {
+    alert("Please enter a number from 1-64");
+    setGridSize();
+  } else {
+    return newSize;
+  }
 }
