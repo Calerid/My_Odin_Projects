29c44023ec9a05388f6861141065b936b23759da
calerid <michael.parker.braley@gmail.com>
2021-03-17 06:44:41 -0500

    Add function outline
    Added the outline to a new setGridSize function. This will take input from getGridSize and create new divs under the gridContainer when completed.

--- etch-a-sketch/scripts/app.js
+++ etch-a-sketch/scripts/app.js
@@ -1,10 +1,18 @@
 const DEFAULT_GRID = 16;
 
 function getGridSize() {
-  var gridSize = document.getElementById("iputGridSize").value;
-  console.log(gridSize);
+  var gridSize = document.getElementById("inputGridSize").value;
   if (gridSize > 64 || gridSize < 1 || isNaN(gridSize)){
     alert("Please enter a valid number between 1 & 64");
     gridSize = DEFAULT_GRID;
+  } else {
+    setGridSize(gridSize);
+  }
+}
+
+function setGridSize(gridSize){
+  let gridContainer = document.getElementById("gridContainer");
+  for (i = DEFAULT_GRID; i < gridSize * gridSize; i++){
+    
   }
 }
